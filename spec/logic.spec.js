da2dfcdacb62b43a8942da31ba0af51f3e502d6a
mdipietr0 <dipietro.michael@gmail.com>
2018-08-11 16:26:55 -0400

    Delete spec file

--- spec/logic.spec.js
+++
@@ -1,9 +0,0 @@
-const {isGameOver} = require('../assets/scripts/games/logic')
-
-describe('Games', function () {
-  // const game = []
-
-  it('determines whether the game is over', function () {
-    isGameOver().should.equal(true)
-  })
-})
